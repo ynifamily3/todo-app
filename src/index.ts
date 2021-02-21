c1d10915dcab99ada6c7deddaf1f1425e0ec02b3
최종근 <miel@choejong-geun-ui-MacBookPro.local>
2021-02-21 15:41:42 +0900

    tree shaking 적용

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { sum } from "./module1";
+import { diff } from "./module2";
 
-console.log(sum(1, 2));
+console.log(sum(1, 2), diff(2, Math.random()));
---
+++ src/module2.ts
@@ -0,0 +1,5 @@
+function diff(a: number, b: number): number {
+  return a - b;
+}
+
+export { diff };
